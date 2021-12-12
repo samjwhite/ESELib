22a860fe6950e01b8ce0114a2333686ac9a0618c
abaporu-C <77457350+abaporu-C@users.noreply.github.com>
2021-12-12 14:50:38 -0500

    Service Order DataSet

---
+++ ESELib/ServiceOrderDataSet.cs
@@ -0,0 +1,18 @@
+﻿namespace ESELib
+{
+
+
+    partial class ServiceOrderDataSet
+    {
+        partial class employeeLookUpDataTable
+        {
+        }
+    }
+}
+
+namespace ESELib.ServiceOrderDataSetTableAdapters {
+    
+    
+    public partial class ServiceOrderViewTableAdapter {
+    }
+}
